84974c1b558609a3485623e817b8ea5b8fb591e9
216501024-byte <216501024@stu.adu.edu.tr>
2026-03-07 00:08:26 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T21:08:18.569Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T21:08:25.979Z
+Commit: Correct typo in configuration file
