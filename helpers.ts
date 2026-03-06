858dd701e514bc82aba6f82b2cca244e144b0fa5
216501024-byte <216501024@stu.adu.edu.tr>
2026-03-07 00:08:30 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T21:08:25.979Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T21:08:30.676Z
+Commit: Resolve null pointer exception in user service
