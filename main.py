59b505ebef902ba57476e2a6c7fe8cf70fed62bd
216501024-byte <216501024@stu.adu.edu.tr>
2026-03-07 00:08:17 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T21:08:09.721Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-06T21:08:17.660Z
+Commit: Patch API endpoint error handling
