6ec58e16f6939717769d3715ac608d43ca02c8ee
216501024-byte <216501024@stu.adu.edu.tr>
2026-03-07 00:09:29 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T21:09:22.995Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T21:09:29.060Z
+Commit: Fix CSS styling bug on mobile devices
