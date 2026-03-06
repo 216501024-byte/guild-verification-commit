d945a2535745ca1161df90ad6ba1ea44068f99f4
216501024-byte <216501024@stu.adu.edu.tr>
2026-03-07 00:08:20 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T21:08:17.660Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T21:08:20.172Z
+Commit: Resolve null pointer exception in user service
